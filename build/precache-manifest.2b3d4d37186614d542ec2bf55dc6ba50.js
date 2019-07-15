2e172d3281ca08a5c13301adc9ab313fa5cabb5f
lizhengtang <lizhengtang@auraxy.com>
2019-07-15 15:41:01 +0800

    feat:

---
+++ build/precache-manifest.2b3d4d37186614d542ec2bf55dc6ba50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28225af97218f366f4f75474beeb8417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b87e4fadf592ad0b0f",
+    "url": "/static/css/2.fb390452.chunk.css"
+  },
+  {
+    "revision": "42f4a10b324ca8f1bfae",
+    "url": "/static/css/main.98482aba.chunk.css"
+  },
+  {
+    "revision": "11b87e4fadf592ad0b0f",
+    "url": "/static/js/2.7264b970.chunk.js"
+  },
+  {
+    "revision": "42f4a10b324ca8f1bfae",
+    "url": "/static/js/main.8e7a99f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
